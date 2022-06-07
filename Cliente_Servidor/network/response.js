21a29291ad7bd881df9531cca7671027bc70170b
edrei01 <84791146+edrei01@users.noreply.github.com>
2022-06-07 08:45:53 -0500

    Add files via upload

---
+++ Cliente_Servidor/network/response.js
@@ -0,0 +1,14 @@
+export function success(req, res,message,status){
+    
+    let statusCode = status || 200;
+    let statusMessage = message || '';
+    let error = false;
+
+    res.status(status).send({
+
+        error,
+        statusCode,
+        statusMessage
+        
+    });
+}
